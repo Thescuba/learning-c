a779cebd78c7d2b1aa76069c0bb7032bf6cb94e6
Steve Tang <stevetang92@gmail.com>
2018-10-09 00:27:06 -0700

    Add  helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+// helloworld.cpp: Steve Tang
+// Description: A program that prints "Hello World!"
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return 0
+}
